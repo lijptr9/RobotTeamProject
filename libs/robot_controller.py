77887c0ca74a9dd0a4dcd2efacea18169220cc86
lijptr9 <lij8@rose-hulman.edu>
2018-05-09 09:01:55 -0400

    CHANGE

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -18,6 +18,27 @@ import time
 
 class Snatch3r(object):
     """Commands for the Snatch3r robot that might be useful in many different programs."""
+
+    def __init__(self):
+        self.left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
+        self.right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
+
+        assert self.left_motor.connected
+        assert self.right_motor.connected
+
+
+    def forward(self, inches, speed=100, stop_action= 'brake'):
+        k = 360/4.5
+        degrees_for_motor_to_run = k* inches / speed
+        self.left_motor.run_to_rel_pos(speed_sp= speed, position_sp= degrees_for_motor_to_run, stop_action= stop_action)
+        self.right_motor.run_to_rel_pos(speed_sp=speed, position_sp=degrees_for_motor_to_run, stop_action=stop_action)
+
+        self.left_motor.wait_while('running')
+        self.right_motor.wait_while('running')
+
+
+
+
     
     # TODO: Implement the Snatch3r class as needed when working the sandox exercises
     # (and delete these comments)
