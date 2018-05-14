6f6277a41231fa428dc329305db915b4caf1f914
lijptr9 <lij8@rose-hulman.edu>
2018-05-14 09:25:31 -0400

    CHANGE

--- sandbox/lijptr9/mqtt/m5_pc_remote_drive.py
+++ sandbox/lijptr9/mqtt/m5_pc_remote_drive.py
@@ -34,7 +34,8 @@ import mqtt_remote_method_calls as com
 def main():
     # TODO: 2. Setup an mqtt_client.  Notice that since you don't need to receive any messages you do NOT need to have
     # a MyDelegate class.  Simply construct the MqttClient with no parameter in the constructor (easy).
-    mqtt_client = None  # Delete this line, it was added temporarily so that the code we gave you had no errors.
+    mqtt_client = com.MqttClient()
+    mqtt_client.connect_to_ev3()
 
     root = tkinter.Tk()
     root.title("MQTT Remote")
@@ -62,24 +63,30 @@ def main():
 
     forward_button = ttk.Button(main_frame, text="Forward")
     forward_button.grid(row=2, column=1)
+    forward_button['command'] = lambda : send_forward(mqtt_client)
     # forward_button and '<Up>' key is done for your here...
     # forward_button['command'] = lambda: some_callback1(mqtt_client, left_speed_entry, right_speed_entry)
     # root.bind('<Up>', lambda event: some_callback1(mqtt_client, left_speed_entry, right_speed_entry))
 
     left_button = ttk.Button(main_frame, text="Left")
     left_button.grid(row=3, column=0)
+    left_button['commmand'] = lambda : send_left(mqtt_client)
     # left_button and '<Left>' key
 
     stop_button = ttk.Button(main_frame, text="Stop")
     stop_button.grid(row=3, column=1)
+    stop_button['command'] = lambda : send_stop(mqtt_client)
     # stop_button and '<space>' key (note, does not need left_speed_entry, right_speed_entry)
 
     right_button = ttk.Button(main_frame, text="Right")
     right_button.grid(row=3, column=2)
+    right_button['command'] = lambda : send_right(mqtt_client)
     # right_button and '<Right>' key
 
     back_button = ttk.Button(main_frame, text="Back")
     back_button.grid(row=4, column=1)
+    back_button['command'] = lambda : send_back(mqtt_client)
+
     # back_button and '<Down>' key
 
     up_button = ttk.Button(main_frame, text="Up")
@@ -107,7 +114,27 @@ def main():
 # ----------------------------------------------------------------------
 # Tkinter callbacks
 # ----------------------------------------------------------------------
-# TODO: 4. Implement the functions for the drive button callbacks.
+# DONE: 4. Implement the functions for the drive button callbacks.
+def send_forward(mqtt_client):
+    print("driving forward")
+    mqtt_client.send_message("Forward")
+
+def send_back(mqtt_client):
+    print("driving backward")
+    mqtt_client.send_message("Backward")
+
+def send_left(mqtt_client):
+    print("turning left")
+    mqtt_client.send_message("Left")
+
+def send_right(mqtt_client):
+    print("turning right")
+    mqtt_client.send_message("Right")
+
+def send_stop(mqtt_client):
+    print("stop action")
+    mqtt_client.send_message("Stop")
+
 
 # TODO: 5. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.  This is the final one!
 #
